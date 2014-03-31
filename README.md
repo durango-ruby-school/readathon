fcf1dd17da6e159415226d6bc4b16b2396f9b29f
Aaron Renner <aaron@animascodelabs.com>
2014-03-31 23:57:31 +0000

    Initial checkin

---
+++ README.md
@@ -0,0 +1,8 @@
+# Readathon
+
+Sample app for Durango Ruby School
+
+## Setup Instructions
+
+1. Copy config/database.example.yml to config/database.yml
+2. Update config/database.yml to work with your local machine
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Readathon::Application.config.secret_key_base = 'f527bbf7b1ec938f8032825435ce3a776f18a0897a1195f7c5cc13ca32a5ad0626b568d56273058eb4810c0fdd2d6c09dd267200b053e7bff31ea464b881c75f'
